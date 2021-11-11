31e64df3ff36ba5c1132ab9187729af6592c4d8b
TomValent <74118015+TomValent@users.noreply.github.com>
2021-11-11 15:17:44 +0100

    FINAL

--- c204/c204.c
+++ c204/c204.c
@@ -29,7 +29,6 @@
 ** Proto zde používáme pojem funkce i pro operace, které by byly
 ** v algoritmickém jazyce Pascalovského typu implemenovány jako procedury
 ** (v jazyce C procedurám odpovídají funkce vracející typ void).
-**
 **/
 
 #include "c204.h"
